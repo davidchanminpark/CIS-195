02c26167b967a1e9665078d9f3ed2f62d084e8bd
Chanmin Park <davidpark0315@gmail.com>
2020-10-09 04:02:42 -0400

    fix submod error

---
+++ tutorials/tutorial3_park_chanmin/tutorial3_park_chanmin/NextViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NextViewController.swift
+//  tutorial3_park_chanmin
+//
+//  Created by David Park on 10/9/20.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+
+}
---
+++ tutorials/tutorial3_park_chanmin/tutorial3_park_chanmin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tutorial3_park_chanmin
+//
+//  Created by David Park on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
